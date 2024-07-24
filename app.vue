754f8cf179120aa9039cefbe2c91d3b43dcd4100
Bohdan <76702178+bohdan-mykhailenko@users.noreply.github.com>
2024-07-24 21:53:03 +0300

    build: add daisy ui

--- app.vue
+++ app.vue
@@ -1,6 +1,16 @@
+<script setup lang="ts">
+const version = 2 + 1;
+</script>
+
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
-  </div>
+  <AppHeader />
+  <div class="hello">Hello Nuxt {{ version }}!</div>
 </template>
+
+<style scoped>
+.hello {
+  font-family: Arial, Helvetica, sans-serif;
+  font-size: 3rem;
+  padding: 2rem;
+}
+</style>
---
+++ components/AppHeader.vue
@@ -0,0 +1,13 @@
+<template>
+  <header class="app-header">Header</header>
+
+  <div class="p-4">
+    <button class="btn">Hello daisyUI</button>
+  </div>
+</template>
+
+<style scoped>
+.app-header {
+  background-color: aqua;
+}
+</style>
